d0848b45e98b0c5377ee8e65baa5a57e8564f0f0
GaetCher <roberu69@gmail.com>
2021-08-05 20:29:59 +0200

    Fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ class KissScrollAnim {
         entries.forEach(entry => {
             if (entry.intersectionRatio > this.ratio) {
                 entry.target.classList.remove('ksa-invisible');
-                entry.target.classList.addcd('ksa-active');
+                entry.target.classList.add('ksa-active');
                 observer.unobserve(entry.target)
             }
         });
